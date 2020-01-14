939de28f408dd9eb210628c512889b9c27401734
pbok <przemyslaw.bok@intive.com>
2020-01-14 17:25:05 +0100

    Configured Gatsby to connect to Contentful, installed package responsible for handling rich text data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv');
+
+if  (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -73,6 +79,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
